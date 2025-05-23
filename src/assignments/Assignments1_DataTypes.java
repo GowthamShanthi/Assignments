7838baf830dd5967c0ef8a24d3a5b606c0895ee2
GowthamShanthi <gowtham.shanthi@gmail.com>
2025-05-23 16:15:27 +0530

    Assignement 1 completed related to data types

---
+++ src/assignments/Assignments1_DataTypes.java
@@ -0,0 +1,102 @@
+package assignments;
+
+public class Assignments1_DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		//Print Temperature of a city in degrees Celsius: 25.5
+
+		float temprature =  25.5f;
+
+		System.out.println( "Temperature of a city in degrees is \t"+temprature);
+
+
+		//Whether a customer has placed an order: true or false
+
+		boolean isorderplaced = true;
+
+		System.out.println( "Whether a customer has placed an order: \t"+isorderplaced);
+
+		// Print Person's phone number: "123-456-7890"
+
+		String PhoneNumber = "123-456-7890";
+
+		System.out.println("The persons Phone numer is\t"+PhoneNumber);
+
+
+		//Print Amount of money in a customer's bank account: 1000.50
+
+		float BankBalance = 1000.50f;
+
+		System.out.println(" Amount of money in a customer's bank account is: \t"+BankBalance);
+
+
+
+		// Print Person's email address: "john.doe@example.com"
+
+		String emailaddress =  "john.doe@example.com";
+
+
+		System.out.println("The person's email address is \t"+emailaddress);
+
+
+
+
+		// print Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+		float latitude =  37.7749f;
+		double longitude  = 122.4194	;
+
+
+
+		System.out.println("Coordinates of a location (latitude, longitude): \t"+latitude+","+longitude);
+
+
+		// Print Person's marital status: true or false
+
+		Boolean maritalstatus = true;
+
+		System.out.println("Person's marital status is: \t"+maritalstatus);
+
+
+		// Print Person's occupation: "Software Engineer"
+
+		String Occupation = "Software Engineer";
+
+		System.out.println("The persons occupation is \t"+Occupation);
+
+
+		// Print Person's favourite colour: "Blue"
+
+		String favcolor = "Blue";
+
+		System.out.println("The peron's fav color is \t"+favcolor);
+
+		//Print Person's blood type: 'A'
+
+		char bloodtype = 'A';
+
+		System.out.println("Person's blood type is : \t "+bloodtype);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
