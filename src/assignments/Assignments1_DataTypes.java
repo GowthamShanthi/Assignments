a829cacf300b1b0f1d0fbca0c8e71dfbf0f39406
GowthamShanthi <gowtham.shanthi@gmail.com>
2025-05-23 16:30:14 +0530

    Assignement 1 completed related to data types - With Updated Data

--- src/assignments/Assignments1_DataTypes.java
+++ src/assignments/Assignments1_DataTypes.java
@@ -10,27 +10,27 @@ public class Assignments1_DataTypes {
 
 		float temprature =  25.5f;
 
-		System.out.println( "Temperature of a city in degrees is \t"+temprature);
+		System.out.println( "1.Temperature of a city in degrees is \t"+temprature);
 
 
 		//Whether a customer has placed an order: true or false
 
 		boolean isorderplaced = true;
 
-		System.out.println( "Whether a customer has placed an order: \t"+isorderplaced);
+		System.out.println( "2.Whether a customer has placed an order: \t"+isorderplaced);
 
 		// Print Person's phone number: "123-456-7890"
 
 		String PhoneNumber = "123-456-7890";
 
-		System.out.println("The persons Phone numer is\t"+PhoneNumber);
+		System.out.println("3.The persons Phone numer is\t"+PhoneNumber);
 
 
 		//Print Amount of money in a customer's bank account: 1000.50
 
 		float BankBalance = 1000.50f;
 
-		System.out.println(" Amount of money in a customer's bank account is: \t"+BankBalance);
+		System.out.println("4.Amount of money in a customer's bank account is: \t"+BankBalance);
 
 
 
@@ -39,7 +39,7 @@ public class Assignments1_DataTypes {
 		String emailaddress =  "john.doe@example.com";
 
 
-		System.out.println("The person's email address is \t"+emailaddress);
+		System.out.println("5.The person's email address is \t"+emailaddress);
 
 
 
@@ -51,36 +51,103 @@ public class Assignments1_DataTypes {
 
 
 
-		System.out.println("Coordinates of a location (latitude, longitude): \t"+latitude+","+longitude);
+		System.out.println("6.Coordinates of a location (latitude, longitude): \t"+latitude+","+longitude);
 
 
 		// Print Person's marital status: true or false
 
 		Boolean maritalstatus = true;
 
-		System.out.println("Person's marital status is: \t"+maritalstatus);
+		System.out.println("7.Person's marital status is: \t"+maritalstatus);
 
 
 		// Print Person's occupation: "Software Engineer"
 
 		String Occupation = "Software Engineer";
 
-		System.out.println("The persons occupation is \t"+Occupation);
+		System.out.println("8.The persons occupation is \t"+Occupation);
 
 
 		// Print Person's favourite colour: "Blue"
 
 		String favcolor = "Blue";
 
-		System.out.println("The peron's fav color is \t"+favcolor);
+		System.out.println("9.The peron's fav color is \t"+favcolor);
+
+
+		// Print Title of a book: "To Kill a Mockingbird"
+
+		String booktitle = "To Kill a Mockingbird";
+
+		System.out.println("10.The title of the book is \t "+booktitle);
+
+		//15.Number of employees in a company: 500
+
+		int noOfEmployees = 500;
+		System.out.println("11.The number of employees in the company is \t"+noOfEmployees);
+
+
+		//  Time of an event: 2:30 PM
+
+		String timeOfEvent = "2.30PM";
+
+		System.out.println("12.The time of an event is \t "+timeOfEvent);
+
+		// Name of a country: "United States"
+
+		String CountryName = "United States";
+
+		System.out.println("13.The country name is "+CountryName);
+
+		// Person's eye color: "Brown"
+
+		String eyeColor = "Brown";
+		System.out.println("14.The eye color is \t"+eyeColor);
+
+		// Person's birthplace: "New York City"
+
+		String birthplace = "New York City";
+		System.out.println("15.The birth place is \t"+birthplace);
+
+
+		// Distance between two cities: 200.5
+
+		float distance = 200.5f;
+		System.out.println("16.The distance between two cities is "+distance);
+
+
+
+
+
 
 		//Print Person's blood type: 'A'
 
 		char bloodtype = 'A';
 
-		System.out.println("Person's blood type is : \t "+bloodtype);
+		System.out.println("17.Person's blood type is : \t "+bloodtype);
 
 
+		// Print  Current year: 2023
+
+		int currentyear = 2023;
+
+		System.out.println("18.The current yesr is \t"+currentyear);
+
+		//Print Number of followers on a social media platform: 1,000,000
+
+		long noOfFollowers = 1000000l;
+
+		System.out.println("19.The no of followers ins ocial media is \t"+noOfFollowers);
+
+
+		// Print Rating of a movie: 7.5
+
+		float movierating = 7.5f;
+		System.out.println("20.The ratin of movie is \t"+movierating);
+
+
+
+		System.out.println();
 
 
 
