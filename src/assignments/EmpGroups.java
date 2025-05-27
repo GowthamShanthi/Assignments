9a5b23ffe4be5415be58b3208fd3b0c429f1c2f9
GowthamShanthi <gowtham.shanthi@gmail.com>
2025-05-27 19:45:00 +0530

    Assignment 3

---
+++ src/assignments/EmpGroups.java
@@ -0,0 +1,27 @@
+package assignments;
+
+public class EmpGroups extends Employees{
+
+
+	String [] empNames = {empname1 ,empname2,empname3   };
+
+	int [] empIds = { empid1,empid2,empid3};
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		EmpGroups emp = new EmpGroups();
+
+
+
+		System.out.println("The first  employee detail is "+emp.empNames[0]+'\t'+emp.empIds[0]);
+		System.out.println("The Second employee detail is "+emp.empNames[1]+'\t'+emp.empIds[1]);
+		System.out.println("The Third employee detail is "+emp.empNames[2]+'\t'+emp.empIds[2]);
+
+
+
+	}
+
+}
