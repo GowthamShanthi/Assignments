90c37fb250925f28c30b856bddf187d0c6225c47
GowthamShanthi <gowtham.shanthi@gmail.com>
2025-08-04 17:18:50 +0530

    Adding Daily practices

---
+++ src/Java_Parctices/DataTypesExample.java
@@ -0,0 +1,35 @@
+package Java_Parctices;
+
+public class DataTypesExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+// 		Numeric data types
+
+		int a = 100, b = 200;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a + b);
+
+		byte by = 120;
+
+		System.out.println(by);
+
+		short sh = 250;
+
+		byte testw;
+		System.out.println(testw);
+		
+		
+		System.out.println(sh);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/Java_Parctices/FirstProgram.java
@@ -0,0 +1,16 @@
+package Java_Parctices;
+
+public class FirstProgram {
+	
+	
+	public static void main(String[] args) {
+		
+	System.out.println("Namachivaya Vaalga");
+	System.out.println("Naathan Thaal Vaalga ");
+	System.out.println("Imai poluthum en nenjil neenga thaan thaal vaalga");
+		
+		
+		
+	}
+
+}
