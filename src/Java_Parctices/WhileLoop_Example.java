462d890006e5b10390ced9de17b8ce00d4bba77e
GowthamShanthi <gowtham.shanthi@gmail.com>
2025-08-06 07:25:21 +0530

    Commiting while loop example

---
+++ src/Java_Parctices/WhileLoop_Example.java
@@ -0,0 +1,54 @@
+package Java_Parctices;
+
+public class WhileLoop_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Example 1: 1...10
+		
+		int i =1; //initialization
+		
+		
+		
+		while(i<=10) // condition
+		
+		{
+			
+			System.out.println("The i value now is"+i);
+
+		++i; // increment
+		
+		}
+		
+		
+		//Example 2 - print "hello" 10times
+		
+		int j = 1;
+		
+		while(j<=0)
+		{
+			
+			System.out.println("Hello");
+		
+		j++;
+		}
+		
+		
+		//Exmaple 3
+		
+		int k =2;
+		
+		while(k<=10)
+		{
+			System.out.println("The k  value is now "+k);
+			
+			k+=2;
+		}
+		
+		
+
+	}
+
+}
