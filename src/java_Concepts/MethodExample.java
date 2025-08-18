27a28bab5312ca6fe1870e1da4431a4497a470d2
GowthamShanthi <gowtham.shanthi@gmail.com>
2025-08-18 10:12:26 +0530

    Adding new files

---
+++ src/java_Concepts/MethodExample.java
@@ -0,0 +1,43 @@
+package java_Concepts;
+
+public class MethodExample {
+	
+	// No Prams No Return value 
+	void m1()
+	{
+		System.out.println("Hello...");
+	}
+
+	
+	//2 No params return value
+	
+	
+	String m2() {
+		
+		return("Hello how are you? ");
+	}
+	
+	
+	//3 - Takes params no return value 
+	
+	void m3(String name)
+	{
+		
+		System.out.println("Hello"+name);
+	}
+	
+	
+	
+	//4 - Takes the parameters returns value 
+	
+String	m4(String name)
+	{
+		
+		return("Hello "+ name);
+		
+		
+	}
+	
+ 
+
+}
---
+++ src/java_Concepts/MethodMain_Exampls.java
@@ -0,0 +1,34 @@
+package java_Concepts;
+
+public class MethodMain_Exampls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MethodExample exmp = new MethodExample();
+		
+		
+		exmp.m1();
+		
+		exmp.m2();
+		
+		String s1 = exmp.m2();
+		
+		System.out.println(s1);
+		
+		
+		exmp.m3("\t Namachivaya");
+		
+		
+		String s2 = exmp.m4("Vaalga Ayya");
+		
+		
+		System.out.println( s2 );
+		
+		s2.length();
+		
+		
+		
+	}
+
+}
